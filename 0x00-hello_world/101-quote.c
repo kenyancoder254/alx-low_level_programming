365001cc2606a478450364e574b59b35c65f08a5
kenyancoder254 <kenyancoder05@gmail.com>
2023-12-17 20:34:06 +0000

    Print a simple statement to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <unistd.h>
+/**
+ * main - prints a statement using write system call in C
+ *
+ * Return: 1 if successful
+ */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, message, sizeof(message) - 1);
 	return (1);
 }
