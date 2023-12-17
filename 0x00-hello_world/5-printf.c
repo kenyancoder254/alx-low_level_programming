1b1b2c89f1721e5927ade34b184fb2d559cf62e9
kenyancoder254 <kenyancoder05@gmail.com>
2023-12-17 19:53:45 +0000

    Print a simple statement using printf() function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a sentence using printf() function in C
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
